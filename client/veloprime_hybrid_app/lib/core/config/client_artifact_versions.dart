8498e966724357b76b495c5f951770944fb8367e
Adrian <adrian.szymborski@gmail.com>
2026-03-31 22:15:25 +0200

    chore: release 0.1.10.4

--- client/veloprime_hybrid_app/lib/core/config/client_artifact_versions.dart
+++ client/veloprime_hybrid_app/lib/core/config/client_artifact_versions.dart
@@ -3,8 +3,8 @@ class ClientArtifactVersions {
 
   static const String data = 'v1';
   static const String assets = 'v1';
-  static const String application = 'v10';
-  static const String release = '0.1.10.3';
+  static const String application = 'v11';
+  static const String release = '0.1.10.4';
 
   static String get releaseLabel => 'wersja $release';
 }
