24000d2693c990190cd2c0806ac920101926f149
Adrian <adrian.szymborski@gmail.com>
2026-04-04 01:02:44 +0200

    Hotfix 0.1.12.16 restore trusted updater signer

--- client/veloprime_hybrid_app/lib/core/config/client_artifact_versions.dart
+++ client/veloprime_hybrid_app/lib/core/config/client_artifact_versions.dart
@@ -3,8 +3,8 @@ class ClientArtifactVersions {
 
   static const String data = 'v1';
   static const String assets = 'v1';
-  static const String application = 'v29';
-  static const String release = '0.1.12.15';
+  static const String application = 'v30';
+  static const String release = '0.1.12.16';
 
   static String syncedDataVersion = data;
   static String syncedAssetsVersion = assets;
